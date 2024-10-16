652bba26d5c00a2b6774cebd3810405b0af195b0
Yumiko-lab <1032816215@qq.com>
2024-10-16 16:57:30 +0800

    add

--- NowCoder/暑期多校/2024/1/h.cpp
+++ NowCoder/暑期多校/2024/1/h.cpp
@@ -2,8 +2,9 @@
 using namespace std;
 
 struct Info {
-    string name;
-    int s, p;
+    // name, problems, penalty, 
+    string s;
+    int p, t;
 };
 
 int main() {
@@ -13,54 +14,64 @@ int main() {
     int n;
     cin >> n;
     vector<Info> a(n);
-    for (auto &[name, s, p] : a) {
-        cin >> name >> s >> p;
+    set<string> S1, S2;
+    for (auto &[s, p, t] : a) {
+        cin >> s >> p >> t;
+        S1.insert(s);
     }
-    sort(a.begin(), a.end(), [](const Info &x, const Info &y) {
-        if (x.s != y.s) {
-            return x.s > y.s;
+    sort(a.begin(), a.end(), [&](auto x, auto y) {
+        if (x.p != y.p) {
+            return x.p > y.p;
         }
-        return x.p < y.p;
+        return x.t < y.t;
     });
 
     int m;
     cin >> m;
     vector<Info> b(m);
-    for (auto &[name, s, p] : b) {
-        cin >> name >> s >> p;
+    for (auto &[s, p, t] : b) {
+        cin >> s >> p >> t;
+        S2.insert(s);
     }
-    sort(b.begin(), b.end(), [](const Info &x, const Info &y) {
-        if (x.s != y.s) {
-            return x.s > y.s;
+    sort(b.begin(), b.end(), [&](auto x, auto y) {
+        if (x.p != y.p) {
+            return x.p > y.p;
         }
-        return x.p < y.p;
+        return x.t < y.t;
     });
 
+
     int rk1 = -1, rk2 = -1;
-    set<string> S1, S2;
     for (int i = 0; i < n; i++) {
-        if (a[i].name == "lzr010506") {
-            rk1 = i + 1;
+        if (a[i].s == "lzr010506") {
+            rk1 = i;
             break;
         }
-        S1.insert(a[i].name);
     }
 
-    int cnt = 0;
     for (int i = 0; i < m; i++) {
-        if (b[i].name == "lzr010506") {
-            rk2 = i + 1;
+        if (b[i].s == "lzr010506") {
+            rk2 = i;
             break;
         }
-        if (S1.count(b[i].name)) {
-            cnt++;
-        }
-        S2.insert(b[i].name);
     }
 
     // 一场比赛可能会有多个同一个学校的队伍参加
+    int ans1 = rk1 + 1, ans2 = rk2 + 1;
 
+    for (int i = 0; i < rk1; i++) {
+        if (S2.count(a[i].s)) {
+            ans1--;
+        }
+    }
 
+    for (int i = 0; i < rk2; i++) {
+        if (S1.count(b[i].s)) {
+            ans2--;
+        }
+    }
+
+    cout << min(ans1, ans2) << '\n';
 
     return 0;
 }
