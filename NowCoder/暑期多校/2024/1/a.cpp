f17d846a917bdfa45a34c64a8c9e6853d0a6c1e8
Yumiko-lab <1032816215@qq.com>
2024-10-16 19:42:16 +0800

    add

---
+++ NowCoder/暑期多校/2024/1/a.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, m, q;
+    cin >> n >> m >> q;
+
+    
+
+    return 0;
+}
