021e3cd46e735ee0d1f202aeadea0132e55f0ff4
Yumiko-lab <1032816215@qq.com>
2024-11-02 12:14:33 +0800

    add Harbin

--- XCPC/03 - CCPC/2024/Harbin/k.cpp
+++ XCPC/03 - CCPC/2024/Harbin/k.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
+using i64 = long long;
 
 struct Info {
     int w, l, r;
@@ -11,20 +12,31 @@ int main() {
 
     int n, m;
     cin >> n >> m;
-    vector<Info> a(n);
-    for (auto &[w, l, r] : a) {
+    vector<Info> a(n + 1);
+    i64 ans = 0;
+    for (int i = 1; i <= n; i++) {
+        int w, l, r;
         cin >> w >> l >> r;
+        a[i] = {w, l, r};
+        ans += 1LL * l * w;
+        m -= l;
     }
 
+    a[0].w = 1e9;
     sort(a.begin(), a.end(), [&](auto x, auto y) {
         return x.w > y.w;
     });
 
-    cout << '\n';
-    for (auto [w, l, r] : a) {
-        cout << w << ' ' << l << ' ' << r << '\n';
+    for (int i = 1; i <= n; i++) {
+        if (m == 0) {
+            break;
+        }
+        int mn = min(m, a[i].r - a[i].l);
+        m -= mn;
+        ans += 1LL * a[i].w * mn;
     }
-    cout << '\n';
+
+
 
     return 0;
 }
