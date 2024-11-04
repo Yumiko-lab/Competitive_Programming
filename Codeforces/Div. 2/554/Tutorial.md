53e6a10858fe2edee46d59a7a946b403d0ef2c1e
Yumiko-lab <1032816215@qq.com>
2024-11-04 17:29:33 +0800

    Codeforces Round 554 (Div. 2) - C

---
+++ Codeforces/Div. 2/554/Tutorial.md
@@ -0,0 +1,34 @@
+# Codeforces Round 554 (Div. 2)
+
+[Codeforces Round 554 (Div. 2)](https://codeforces.com/contest/1152) 
+
+
+
+## A. 
+
+
+## B. 
+
+
+
+## C. 
+
+
+## D. 
+
+
+## E. 
+
+
+## F1. 
+
+
+
+
+## F2. 
+
+
+
+
+
+
--- Codeforces/Div. 2/554/c.cpp
+++ Codeforces/Div. 2/554/c.cpp
@@ -10,8 +10,31 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    // Classcal Number Theory Problem
-    
+    int a, b;
+    cin >> a >> b;
+    if (a < b) {
+        swap(a, b);
+    }
 
+    array<i64, 2> ans{0, lcm(a, b)}; // {k = 0, lcm(a, b)}
+    auto calc = [&](auto f) { // factor
+        i64 k = f - (a % f), l = lcm(a + k, b + k);
+        if (l < ans[1]) {
+            ans = {k, l};
+        }
+    };
+
+    int d = a - b;
+    for (int i = 1; i <= d / i; i++) {
+        if (d % i == 0) {
+            calc(i);
+            if (i != d / i) {
+                calc(d / i);
+            }
+        }
+    }
+
+    cout << ans[0] << '\n';
+    
     return 0;
 }
