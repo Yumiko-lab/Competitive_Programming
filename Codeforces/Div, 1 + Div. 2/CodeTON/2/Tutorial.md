e3bbfc9f0f7590f0d0bbee44cfa98250450e836b
hbq2004 <1032816215@qq.com>
2024-11-09 21:17:42 +0800

    add CodeTON 2

---
+++ Codeforces/Div, 1 + Div. 2/CodeTON/2/Tutorial.md
@@ -0,0 +1,4 @@
+# CodeTON 2
+
+[CLIST - Problems Rating Distribution](https://clist.by/problems/?resource=1&contest=36100181) 
+
---
+++ Codeforces/Div, 1 + Div. 2/CodeTON/2/c.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int n, m;
+    cin >> n >> m;
+    vector<int> a(m);
+    for (auto &x : a) {
+        cin >> x;
+    }
+    
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
