3b00a8d4813e695b030227d8cfd765ebaf934669
hbq2004 <1032816215@qq.com>
2024-11-13 10:27:14 +0800

    add 2022 HDU 2

---
+++ Training Camps/Multi University Training Contest (HDU)/2022/2/b.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+
+void solve() {
+    int n;
+    i64 H;
+    cin >> n >> H;
+    
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
