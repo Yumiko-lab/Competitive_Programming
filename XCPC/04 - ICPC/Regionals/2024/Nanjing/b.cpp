a45476cc556383302551a44cc467cdf0faca0923
hbq2004 <1032816215@qq.com>
2024-11-25 17:27:03 +0800

    add problems

--- XCPC/04 - ICPC/Regionals/2024/Nanjing/b.cpp
+++ XCPC/04 - ICPC/Regionals/2024/Nanjing/b.cpp
@@ -2,22 +2,7 @@
 using namespace std;
 
 void solve() {
-    string s;
-    cin >> s;
-    int n = s.size();
-    string t;
-    for (int i = 0; i < n; i++) {
-        int j = i;
-        while (j < n && s[j] == s[i]) {
-            j++;
-        }
-        if (j - i & 1) {
-            t += s[i];
-        }
-        i = j - 1;
-    }
-    // cout << t << '\n';
-    cout << t.size() << ' ' << t << '\n';
+    
 }
 
 int main() {
